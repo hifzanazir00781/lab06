f4a4408bc7a79e4a056928dae0d3b29cf2ce6a42
hifza <hifzanazir456@gmail.com>
2023-12-21 21:49:20 +0500

    by using if else if condition write the conditions for Average, lightness, luminosity

--- colorUtils.c
+++ colorUtils.c
@@ -16,8 +16,19 @@ int toGrayScale(int *r, int *g, int *b, Mode mode) {
   {
     return Error;
   }
-  
-  
+  if(mode == AVERAGE)
+  {
+    *r = *g = *b = (*r + *g + *b)/3;
+  }
+  else if (mode == LIGHTNESS)
+  {
+    *r = *g = *b = (max(*r , *g , *b) + min(*r , *g , *b))/2;
+  }
+  else if(mode == LUMINOSITY)
+  {
+    *r = *g = *b = (0.21 * *r + 0.72 * *g + 0.07 * *b);
+  }
+  return No_Error;
   
 }
 
